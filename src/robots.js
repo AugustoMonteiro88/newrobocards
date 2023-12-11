797edc81639ff62c5e2e0191e37f135314be1ca3
AugustoMonteiro88 <guto_pg@terra.com.br>
2023-12-11 01:25:28 -0500

    first commit

---
+++ src/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+
+	{
+		id: 1,
+		name: 'John Maclaine',
+		username: 'jmclane',
+		email: 'john.maclaine@nakatomi.com'
+	},
+	{
+		id: 2,
+		name: 'Michael Scott',
+		username: 'mscott',
+		email: 'michael.scott@dundermifflin.com'
+	},
+	{
+		id: 3,
+		name: 'Chanandler Bong',
+		username: 'mschanbong',
+		email: 'tvguidelady@friends.us'
+	},
+	{
+		id: 4,
+		name: 'Massa Barrichelo',
+		username: 'mbarrica',
+		email: 'f1br@fia.dapu'
+	},
+	{
+		id: 5,
+		name: 'Junileu Gervais',
+		username: 'jgervais',
+		email: 'jgmano@mano.com'
+	}
+]
