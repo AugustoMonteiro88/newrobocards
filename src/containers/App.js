7ee3376a25a1ebc72895f49b3f07d3340f6ddeb5
AugustoMonteiro88 <guto_pg@terra.com.br>
2025-03-12 07:41:39 -0400

    converting class components into functional

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React , { Component } from 'react'
+import React , { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import './App.css';
@@ -7,47 +7,50 @@ import ErrorBoundary from '../components/ErrorBoundary'
 // import { robots } from './robots' -> going with jsonplaceholder
 
 
-class App extends Component {
+function App() {
+	// constructor(){
+	// 	super();
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: ''
+	// 	}
+	// }
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState('');
 
-	constructor(){
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 		.then(response => response.json())
+	// 		.then(users => this.setState({ robots: users }))
+	// 	;
+	// }
+	useEffect( () => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => {setRobots(users)});
+	},[]) //simulating componentDidMount with that empty array
+
+	const onSearchChange = (event) => {
+		//this.setState({ searchfield: event.target.value })
+		setSearchfield(event.target.value)
 	}
-
-componentDidMount() {
-	fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => this.setState({ robots: users }))
-	;
-}
-
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
-
-	render() {
-		const { robots, searchfield } = this.state; //destructuring
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		})
-		return !robots.length ?
-			<h1 className ="tc">Loading...</h1> :
-			(		
-				<div className='tc'>
-					<h1 className = 'f1'>RoboCards</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<ErrorBoundary>
-							<CardList robots={filteredRobots}/>
-						</ErrorBoundary>
-					</Scroll>
-				</div>
-			);
-			
-	}
+	//const { robots, searchfield } = this.state; //destructuring
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	})
+	return !robots.length ?
+		<h1 className ="tc">Loading...</h1> :
+		(		
+			<div className='tc'>
+				<h1 className = 'f1'>RoboCards</h1>
+				<SearchBox searchChange={onSearchChange}/> 
+				<Scroll>
+					<ErrorBoundary>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundary>
+				</Scroll>
+			</div>
+		);	
 }
 
 export default App;
